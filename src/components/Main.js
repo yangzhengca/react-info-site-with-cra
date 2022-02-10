2449f51a2ebecaf40e50658a47d29779e134a73b
yangzhengca <yangzhengca>
2022-02-10 16:27:15 -0600

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+
+const Main = () => {
+    return(
+        <h1>Main goes here!</h1>
+    )
+}
+
+export default Main
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import Logo from "../images/logo.png"
+const Navbar = () => {
+    return(
+        <div className="nav">
+            
+                <img src={Logo} />
+            
+            <div className="nav-item">
+                <h4>React Course - Project 1</h4>
+            </div>
+            
+        </div>
+    )
+};
+
+export default Navbar;
