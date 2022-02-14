975bfb804c78315c6ebc43eeaa3d16aea29d2c0e
yangzhengca <yangzhengca>
2022-02-14 13:23:09 -0600

    finished

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,18 @@
 
 const Main = () => {
     return(
-        <h1>Main goes here!</h1>
+        <div className="main">
+            <h1 className="title">
+                Fun facts about React
+            </h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps,including mobile apps</li>
+            </ul>
+        </div>
     )
 }
 
